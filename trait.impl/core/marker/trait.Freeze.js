eb295ea3e1c62167a4e59726dccdd20b7a6b26d6
equation314 <equation618@gmail.com>
2025-03-07 12:15:31 +0000

    Deploying to gh-pages from @ arceos-org/flatten_objects@f066fe65e88f3aa3ffd6ccd286d5330817cfb723 🚀

---
+++ trait.impl/core/marker/trait.Freeze.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["flatten_objects",[["impl&lt;T, const CAP: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/core/primitive.usize.html\">usize</a>&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"flatten_objects/struct.FlattenObjects.html\" title=\"struct flatten_objects::FlattenObjects\">FlattenObjects</a>&lt;T, CAP&gt;",1,["flatten_objects::FlattenObjects"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[490]}
